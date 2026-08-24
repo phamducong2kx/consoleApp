d8ee3e7a225b6dedcc1e798d16af76fadbdfbdc1
phamducong2kx <malzaharvn1230@gmail.com>
2026-08-24 20:28:54 +0700

    update ham main

--- GithubFoloow_2026/Program.cs
+++ GithubFoloow_2026/Program.cs
@@ -3,5 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello từ nhánh feature-dev");
+        Console.WriteLine("day la dau vay cac bro");
+        Console.WriteLine("day la dau vay cac bro lan 2");
     }
 }
