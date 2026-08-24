a4f1d1f5635fbaba8bfa27874926f3dcbf6882ed
phamducong2kx <malzaharvn1230@gmail.com>
2026-08-24 15:35:59 +0700

    add them 1 file class enemies

---
+++ GithubFoloow_2026/Enemies.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubFoloow_2026
+{
+    public class Enemies
+    {
+        int hp;
+        public Enemies(int _hp)
+        {
+            hp = _hp;
+        }
+    }
+}
